018ea2ef00c9110e36a942710c04b1aee2b37da1
OleaBlossom <olea.plum@gmail.com>
2021-11-10 10:59:47 +0100

    10.11.2021

--- Main.java
+++ Main.java
@@ -1,1 +1,9 @@
 
+public class Main {
+
+  public static void main(String[] args) {
+    Mitarbeiter myObj = new Mitarbeiter();
+    myObj.setId(15);
+    System.out.println(myObj.getId());
+  }
+}
---
+++ Mitarbeiter.java
@@ -0,0 +1,17 @@
+/* Erstellen und testen Sie anschließend folgende Klasse Mitarbeiter:
+
+getter and setter for each property
+toString */
+
+public class Mitarbeiter {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId( int $newId) {
+        id = $newId;
+    }
+}
