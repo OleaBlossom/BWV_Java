d40dd8edaa16b91f68168f1fd72aea1771d1c714
OleaBlossom <olea.plum@gmail.com>
2021-11-17 11:50:44 +0100

    name setter and getter

--- Mitarbeiter.java
+++ Mitarbeiter.java
@@ -3,9 +3,25 @@
 getter and setter for each property
 toString */
 
-public class Mitarbeiter {
+public class Mitarbeiter
+{
     private int id;
     private String name;
+
+    public Mitarbeiter(String $newName, int $newId)
+    {
+        setId($newId)
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String $newName)
+    {
+        name = $newName;
+    }
 
     public int getId() {
         return id;
