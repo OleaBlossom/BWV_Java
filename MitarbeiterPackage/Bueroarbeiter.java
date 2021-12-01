49cea11522cf2e96d7330318f6d096db1b39b614
plum_ole <olea.plum@gmail.com>
2021-12-01 12:21:19 +0100

    remove unnecessary dollar signs before variable names

--- MitarbeiterPackage/Bueroarbeiter.java
+++ MitarbeiterPackage/Bueroarbeiter.java
@@ -1,18 +1,13 @@
 package MitarbeiterPackage;
 
-public class Schichtarbeiter
+public class Bueroarbeiter
         extends Mitarbeiter {
-
-    public Schichtarbeiter(String newName, int newId) {
+    public Bueroarbeiter(String newName, int newId) {
         super(newName, newId);
     }
 
     @Override
     protected void setId(int newId) {
-        if (newId < 0) {
-            newId = newId * -1;
-        }
-
         newId %= 1000;
 
         super.setId(newId + 3000);
--- MitarbeiterPackage/Mitarbeiter.java
+++ MitarbeiterPackage/Mitarbeiter.java
@@ -7,32 +7,32 @@ public class Mitarbeiter {
     private int id;
     private String name;
 
-    public Mitarbeiter(String $newName, int $newId) {
-        setId($newId);
-        setName($newName);
+    public Mitarbeiter(String newName, int newId) {
+        setId(newId);
+        setName(newName);
     }
 
     public String getName() {
         return name;
     }
 
-    protected void setName(String $newName) {
-        name = $newName;
+    protected void setName(String newName) {
+        name = newName;
     }
 
     public int getId() {
         return id;
     }
 
-    protected void setId(int $newId) {
-        if ($newId < 0) {
-            $newId = $newId * -1;
+    protected void setId(int newId) {
+        if (newId < 0) {
+            newId = newId * -1;
         }
 
-        if ($newId < 1000) {
-            $newId = $newId + 1000;
+        if (newId < 1000) {
+            newId = newId + 1000;
         }
 
-        id = $newId;
+        id = newId;
     }
 }
