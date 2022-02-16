0bdc1c4793e29ffd7a3221e41ca629228a555e19
plum_ole <olea.plum@gmail.com>
2022-02-16 13:31:11 +0100

    implement setters with validation in Gps

--- src/GpsPackage/Gps.java
+++ src/GpsPackage/Gps.java
@@ -5,7 +5,31 @@ public class Gps {
     private double breitengrad;
 
     public Gps(double laengengrad, double breitengrad) {
-        this.laengengrad = laengengrad;
-        this.breitengrad = breitengrad;
+        setLaengengrad(laengengrad);
+        setBreitengrad(breitengrad);
+    }
+
+    public double getLaengengrad() {
+        return laengengrad;
+    }
+
+    public void setLaengengrad(double laengengrad) {
+        if (laengengrad >= 0 && laengengrad <= 360) {
+            this.laengengrad = laengengrad;
+        } else {
+            throw new IllegalArgumentException("longitude must be between 0 and 360");
+        }
+    }
+
+    public double getBreitengrad() {
+        return breitengrad;
+    }
+
+    public void setBreitengrad(double breitengrad) {
+        if (breitengrad >= -90 && breitengrad <= 90) {
+            this.breitengrad = breitengrad;
+        } else {
+            throw new IllegalArgumentException("latitude must be between the south pole (-90) and north pole (90)");
+        }
     }
 }
